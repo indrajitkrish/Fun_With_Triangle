b4d5c01c1ffa1e1a2529bee59ddd755ed6468453
indrajitkrish <indrajitmanian@gmail.com>
2022-08-24 16:20:10 +0530

    adding expection cases

--- is_triangle.js
+++ is_triangle.js
@@ -4,9 +4,16 @@ var output = document.querySelector("#output");
 output.style.display="none";
 
 function sumoff(angle1,angle2,angle3){
+    if(angle1>0 && angle2>0 && angle3>0)
+    {
    var sum=angle1+angle2+angle3;
    console.log(sum);
    return sum;
+    }
+    else{
+        output.innerText = "Enter all the three angles";
+        return 0;
+    }
 }
 function calculate_Is_a_triangle(){
     var sum = sumoff(Number(angles[0].value),Number(angles[1].value),Number
