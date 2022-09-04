f6df27f9de7601e4a2f4ac957dcb928d4e1acb9b
indrajitkrish <indrajitmanian@gmail.com>
2022-09-04 11:59:44 +0530

    Added expections

--- area.js
+++ area.js
@@ -6,9 +6,17 @@ output.style.display="none";
 function calculate_hypotenuse(){
     var a = Number(lengths[0].value);
     var b = Number(lengths[1].value);
+    if(a >0 && b >0)
+    {
     var sum = a * b;
     var area = (sum/2).toFixed(2) ;
     output.style.display="block";
     output.innerText = "Area of the triangle " + area;
+}
+else
+{
+output.style.display="block";
+output.innerText = "Please enter Base and Height";
+}
 }
 button.addEventListener("click",calculate_hypotenuse);
--- hypotenuse.js
+++ hypotenuse.js
@@ -6,9 +6,19 @@ output.style.display="none";
 function calculate_hypotenuse(){
     var a = Number(lengths[0].value);
     var b = Number(lengths[1].value);
+    if(a >0 && b >0)
+    {
     var sum = (a*a)+(b*b);
     var hyp = Math.sqrt(sum).toFixed(2);
     output.style.display="block";
     output.innerText = "The length of Hypotenuse " + hyp;
+    }
+    else
+{
+    output.style.display="block";
+    output.innerText = "Please enter length of 2 sides";
 }
+}
+
+
 button.addEventListener("click",calculate_hypotenuse);
